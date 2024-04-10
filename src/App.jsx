546ad19a2d2b0b8e045b6ebc67c83ee21a2ba153
Tsipora Enkaoua <tsipoenkaoua@gmail.com>
2024-04-10 11:48:11 +0200

    reverse data flux : flux enfant->parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   
- const hello="Bonjour";
+ const [isAccepted, setIsAccepted] = useState(false); // lors de l'initialisation isAccepted= false
+
   return (
     <>
-    <Try hello={hello}/>
+    <Checkbox isChecked={isAccepted} onCheck={setIsAccepted}/> {/* on envoit aussi la methode qui mets a jour la variable comme ca le flux de donnée va de enfant parent */}
+    <button disabled={!isAccepted}>Envoyer le formulaire</button> {/* desactive le bouton si on n'a pas isAccepted a true */}
     
     </>
   )}
 
-  function Try({hello}){
+
+  function Checkbox({isChecked, onCheck}){
+    
     return(
       <>
-       {hello}
+      <label>
+       <input type="checkbox" checked={isChecked} onChange={(e)=> onCheck(e.target.checked)}/>
+       Accepter les conditions
+       </label> 
       </>
     )
   }
